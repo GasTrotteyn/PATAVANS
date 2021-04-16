e1722d3b2679b8c2ab1da979e6487e25bcf7cc65
Gastón Trotteyn <you@example.com>
2021-04-16 17:26:09 -0300

    form stiled and sending request

--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -3,6 +3,8 @@ import classes from "./AboutUs.module.css";
 import Header from "../../containers/Headers/HeaderStandard/Header";
 import Cta from "../../components/Cta/Cta";
 import lagoon from "../../Assests/optimized/banner-about.jpg";
+import RightPiece from "../../components/Pieces/RightPiece/RightPiece";
+import pataWorking from "../../Assests/optimized/img-about.png";
 
 const About = (props) => {
     return (
@@ -13,6 +15,16 @@ const About = (props) => {
                 flag="ABOUT PATA"
                 title="Know the legend"
             ></Header>
+            <RightPiece
+                flag="ABOUT ME"
+                title="I'm PATA"
+                paragraph1="Juan Manuel Trotteyn (aka ‘Pata’) was born and raised in Mendoza, Argentina. His father introduced him to mountaineering at a young age, but he quickly fell in love with sport climbing and started competing."
+                paragraph2="At 18 years, he won Nationals and shortly after started route-setting for competitions. After his very first trip to Spain, he realized that’s where he wanted to live. For twelve years, Pata was based out of Cornudella de Montsant, Cataluña, the heart of Spain’s most stunning sport climbing. He became involved in the local bolting community, establishing new routes as rebolting the classics. Today, Pata lives in Boulder, CO and enjoys building custom van conversions, making other people’s dream of living in a van life come true. In his free time, Pata climbs on rock or flies his paraglider."
+                srcPhoto={pataWorking}
+                altPhoto="Pata working in a"
+                noButton
+            ></RightPiece>
+
             <Cta></Cta>
         </div>
     );
--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Contact.module.css";
 import Header from "../../containers/Headers/HeaderStandard/Header";
-import Cta from "../../components/Cta/Cta";
 import thistles from "../../Assests/optimized/banner-contact.jpg";
+import axios from "axios";
 
 const Contact = (props) => {
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        tel: "",
+        message: "",
+    });
+
+    const inputChangeHandler = (event, id) => {
+        // event.preventDefault();
+        let formModified = { ...data };
+        formModified[id] = event.target.value;
+        setData(formModified);
+        console.log(data[id]);
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        let newForm = { ...data };
+        axios
+            .post("https://jsonplaceholder.typicode.com/posts", newForm)
+            .then((resp) => console.log(resp));
+    };
+
     return (
         <div className={classes.container}>
             <Header
@@ -13,7 +36,68 @@ const Contact = (props) => {
                 flag="CONTACT"
                 title="We are here to help on your Journey"
             ></Header>
-            <Cta></Cta>
+            <div className={classes.dataSection}>
+                <div className={classes.ourInfo}>
+                    <h4>CONTACT US</h4>
+                    <h2>We are in Denver's area</h2>
+                    <p>
+                        Please contact us with any questions about building your
+                        custom van conversion, and start living your dreamed van
+                        life!
+                    </p>
+
+                    <div className={classes.ourData}>
+                        <p>
+                            Phone: <br></br> E-mail: <br></br>Facebook:{" "}
+                            <br></br>Instagram:
+                        </p>
+
+                        <p className={classes.strongs}>
+                            <strong>9999-9999-999</strong>
+                            <br></br>
+                            <strong>patavansboulder@gmail.com</strong>
+                            <br></br>
+                            <strong>@patavansboulder</strong>
+                            <br></br>
+                            <strong>@patavans</strong>
+                        </p>
+                    </div>
+                </div>
+                <div className={classes.yourInfo}>
+                    <h4>WRITE US</h4>
+                    <form onSubmit={submitHandler}>
+                        <input
+                            placeholder="Name & lastname"
+                            onChange={(event) => {
+                                inputChangeHandler(event, "name");
+                            }}
+                            value={data.name}
+                        ></input>
+                        <input
+                            placeholder="E-mail"
+                            onChange={(event) => {
+                                inputChangeHandler(event, "email");
+                            }}
+                            value={data.email}
+                        ></input>
+                        <input
+                            placeholder="Phone"
+                            onChange={(event) => {
+                                inputChangeHandler(event, "tel");
+                            }}
+                            value={data.tel}
+                        ></input>
+                        <textarea
+                            placeholder="Message"
+                            onChange={(event) => {
+                                inputChangeHandler(event, "message");
+                            }}
+                            value={data.message}
+                        ></textarea>
+                        <button className={classes.button}>Send</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
